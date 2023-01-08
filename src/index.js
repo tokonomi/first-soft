242c44442dd5802891d9702eadeaf4fa55db40db
Walid <foowie99@gmail.com>
2023-01-08 18:55:25 +0400

    setted pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createRoot } from "react-dom/client";
+import LivecycleSdk from '@livecycle/sdk'
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
+LivecycleSdk.init()
 createRoot(document.querySelector("#root")).render(
     <BrowserRouter>
         <App/>
